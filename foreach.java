8748ac7b4791cbfbee04899d761862f5cca98d69
Karishma200504 <shaikskarishma9@gmail.com>
2024-12-10 14:46:55 +0530

    Add files via upload

---
+++ foreach.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class foreach{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a[]={"apple","orange","pine apple"};
+        for(String b:a)
+        System.out.print(b+",");
+    }
+}
